63a95f34d12f44a908607e29e38c877c9ef91aad
Vishal Bharam <vbharam@gmail.com>
2016-10-27 15:33:36 -0700

    Include music player in the background

--- Pokedex/Pokedex/ViewController.swift
+++ Pokedex/Pokedex/ViewController.swift
@@ -35,7 +35,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         parsePokemonCSV()
         
         // Music Player init:
-        // initAudio()
+         initAudio()
     }
     
     
