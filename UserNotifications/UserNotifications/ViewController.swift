691d24285a8175177d9c83cd04c7b23e1f3c0447
Vishal Bharam <vbharam@gmail.com>
2016-11-12 13:13:24 -0800

    Simple User-Notification App

---
+++ UserNotifications/UserNotifications/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserNotifications
+//
+//  Created by Vishal Bharam on 10/29/16.
+//  Copyright © 2016 Codecoop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 1: REQUEST PERMISSION:
+        
+    }
+
+}
